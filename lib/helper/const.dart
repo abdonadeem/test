24b07b149f38cdbb7dd1a06ac826aad30b09abae
abdonadeem <44233227+abdonadeem@users.noreply.github.com>
2024-06-03 19:25:15 +0300

    Delete lib directory

--- lib/helper/const.dart
+++
@@ -1,6 +0,0 @@
-import 'package:woocommerce_app/helper/woocommerce_api.dart';
-
-class Const {
-  static WooCommerceAPI wc_api = new WooCommerceAPI(
-      "your_host_name", "your_consumer_key", "your_secret_key");
-}
